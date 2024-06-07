1679e5e3d08a60fb49879eecb8b4d31b87d8b4e0
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-06 21:45:59 -0700

    Initialized

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Project-RF-Demo
  remaster
+Here is the design doc:
+
+https://docs.google.com/document/d/1ppvF-D36yc-RDPN7SgM-q3T6qqQK2Ur3wmR1_1QK1g4/edit?usp=sharing
---
+++ src/ServerScriptService/ServerScript.server.lua
@@ -0,0 +1,13 @@
+print("hello world from VSCode!")
+
+local function testFunction()
+    print("this is a test function")
+end
+
+testFunction()
+
+local testCFrame = CFrame.new()
+
+
+
+
