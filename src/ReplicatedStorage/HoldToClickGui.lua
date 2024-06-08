ae7592008a4f44d3efe3eab26632b4b75a5a2a72
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-08 16:23:03 -0700

    HoldToClickGui OOP in progress

--- src/ReplicatedStorage/HoldToClickGui.lua
+++ src/ReplicatedStorage/HoldToClickGui.lua
@@ -3,21 +3,47 @@ HoldToClickGui.__index = HoldToClickGui
 ---------------------------------------------local-field-variables-----------------------------------------------------------------------------------------------
 local RunService = game:GetService("RunService")
 local fullBar = UDim2.new(1,0,1,0)
-local drainedBar = UDim2.new(0,0,1,0)
-local guiObject : GuiBase = nil
+local drainedBar = nil
 
-local progressBar = guiObject:FindFirstChildWhichIsA("Frame")
-local fillConnection = nil
+local guiObject : GuiBase = nil
+local progressBar : Frame= nil
+local fillConnection = nil	
 local drainConnection = nil
 local timeAccumulated = 0
+local positionAdjustment = nil
 ---------------------------------------------local-functions-----------------------------------------------------------------------------------------------
+
+local function adjustPosition()
+	if drainedBar == UDim2.new(1,0,0,0) then --vertical
+		progressBar.Position = UDim2.new(0, 0, 1 - progressBar.Size.Height.Scale, 0)
+	elseif drainedBar == UDim2.new(0,0,1,0) then --horizontal
+		progressBar.Position = UDim2.new(1 - progressBar.Size.Height.Scale, 0, 0, 0)
+	end
+end
 
 ---------------------------------------------main-function-----------------------------------------------------------------------------------------------
 HoldToClickGui.timeToFull = nil
 
-function HoldToClickGui.new(guiObj : GuiBase, time : number)
+function HoldToClickGui.new(guiObj : GuiBase, time : number, fillType : number, fillDirection : number)
 	guiObject = guiObj
 	HoldToClickGui.timeToFull = time
+	progressBar = guiObject:FindFirstChild("ProgressBar", true)
+
+	if fillType == 1 then --for filling horizontally
+		drainedBar = UDim2.new(0,0,1,0)
+	elseif fillType == 2 then --for filling vertically
+		drainedBar = UDim2.new(1,0,0,0) 
+	else
+		warn("Invalid fill type parameter: can only be 1 or 2")
+	end
+
+	if fillDirection == 1 then
+		positionAdjustment = false
+	elseif fillDirection == 2 then
+		positionAdjustment = true
+	else
+		warn("invalid fill direction parameter: can only be 1 or 2 ")
+	end
 end
 
 function HoldToClickGui:Start()
@@ -36,6 +62,9 @@ function HoldToClickGui:Start()
 		end
 		timeAccumulated = math.clamp(timeAccumulated + (dt/HoldToClickGui.timeToFull), 0, 1)
 		progressBar.Size = drainedBar:Lerp(fullBar, timeAccumulated)
+		if positionAdjustment then
+			adjustPosition()
+		end
 	end)
 end
 
@@ -51,6 +80,9 @@ function HoldToClickGui:End()
 		end
 		timeAccumulated = math.clamp(timeAccumulated - (dt/HoldToClickGui.timeToFull), 0, 1)
 		progressBar.Size = drainedBar:Lerp(fullBar, timeAccumulated)
+		if positionAdjustment then
+			adjustPosition()
+		end
 	end)
 end
 
--- src/StarterPlayerScripts/CustomProximityPrompt.client.lua
+++ src/StarterPlayerScripts/CustomProximityPrompt.client.lua
@@ -1,14 +1,30 @@
 local ProximityPromptService = game:GetService("ProximityPromptService")
 local squarePPUI = game.ReplicatedStorage:WaitForChild("CustomProximityPromptUI"):WaitForChild("SquareProximityPrompt")
+local HoldToClickGui = require(game:GetService("ReplicatedStorage"):WaitForChild("RojoManaged_RS"):WaitForChild("HoldToClickGui"))
 
 ProximityPromptService.PromptShown:Connect(function(prompt)
 	squarePPUI:Clone().Parent = prompt.Parent
 end)
 
 ProximityPromptService.PromptHidden:Connect(function(prompt)
-	prompt.Parent:FindFirstChild("customPPUI"):Destroy()
+	prompt.Parent:FindFirstChild("SquareProximityPrompt"):Destroy()
 end)
 
 ProximityPromptService.PromptTriggered:Connect(function(prompt, plr)
-	prompt.Parent:FindFirstChild("customPPUI").TextLabel.BorderSizePixel = 4
+	local customPP = prompt.Parent:FindFirstChild("SquareProximityPrompt")
+	customPP.Frame.BackgroundTransparency = 0.5
+end)
+
+--[[
+***The code below doesn't work because the HoldToClickGui wasn't coded in luau OOP correctly.
+	This is a good opportunity to brush up on my Lua OOP
+]]
+ProximityPromptService.PromptButtonHoldBegan:Connect(function(prompt, playerWhoTriggered)
+	local customPP = prompt.Parent:FindFirstChild("SquareProximityPrompt")
+	local ProgressBar = HoldToClickGui.new(customPP, prompt.HoldDuration, 2, 2)
+	ProgressBar:Start()
+
+	ProximityPromptService.PromptButtonHoldEnded:Once(function(prompt, playerWhoTriggered)
+		ProgressBar:End()
+	end)
 end)
