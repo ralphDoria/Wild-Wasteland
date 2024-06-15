9dd8605cc28868602c7754b6955739e4273ab456
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-15 01:04:04 -0700

    Fixed teleporting tool bug

---
+++ src/StarterCharacterScripts/AttachBodyAttach.client.lua
@@ -0,0 +1,19 @@
+--[[
+    !!!
+    Let me not speak too soon, but this should fix the teleporting tool bug.
+]]
+
+local player = game:GetService("Players").LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+character.ChildAdded:Connect(function(child)
+    if child:IsA("Tool") and child:FindFirstChild("BodyAttach") then
+		local bodyAttachJoint : Motor6D = character:FindFirstChild("BodyAttachJoint", true)
+        local bodyAttach : BasePart = child:FindFirstChild("BodyAttach", true)
+        if bodyAttachJoint and bodyAttach then
+            bodyAttachJoint.Part1 = bodyAttach
+        else
+            warn("Unable to attach tool to character: BodyAttachJoint or BodyAttach not found")
+        end
+	end
+end)
