cbd314c3095f328c395be0ef4ff5993b53fb01ff
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-15 05:50:02 -0700

    Update Crouch.lua

--- src/StarterCharacterScripts/CustomMovementWithSFX/Modules/Crouch.lua
+++ src/StarterCharacterScripts/CustomMovementWithSFX/Modules/Crouch.lua
@@ -4,14 +4,17 @@ local player = game.Players.LocalPlayer --Because of this line, this module must
 local character = player.Character
 local humanoid = character:WaitForChild("Humanoid")
 
+local defaultSpeed = game:GetService("StarterPlayer").CharacterWalkSpeed
+local crouchSpeed = 3
+
 local Crouch = {CROUCH_KEY = CROUCH_KEY}
 
 function Crouch.crouchKeyDown()
-    
+    humanoid.WalkSpeed = crouchSpeed
 end
 
 function Crouch.crouchKeyUp()
-    
+    humanoid.WalkSpeed = defaultSpeed
 end
 
 return Crouch
