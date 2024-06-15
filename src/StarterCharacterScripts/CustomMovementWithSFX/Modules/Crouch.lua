59f6227e77f4c87c4a1ea0fe0b78c4eb15a053bd
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-15 05:46:19 -0700

    Update Crouch.lua

--- src/StarterCharacterScripts/CustomMovementWithSFX/Modules/Crouch.lua
+++ src/StarterCharacterScripts/CustomMovementWithSFX/Modules/Crouch.lua
@@ -1,5 +1,17 @@
 local CROUCH_KEY = Enum.KeyCode.C
 
+local player = game.Players.LocalPlayer --Because of this line, this module must be required in a LocalScript, else there will be an error.
+local character = player.Character
+local humanoid = character:WaitForChild("Humanoid")
+
 local Crouch = {CROUCH_KEY = CROUCH_KEY}
+
+function Crouch.crouchKeyDown()
+    
+end
+
+function Crouch.crouchKeyUp()
+    
+end
 
 return Crouch
