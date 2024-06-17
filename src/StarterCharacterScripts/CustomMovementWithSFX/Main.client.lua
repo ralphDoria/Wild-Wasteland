e9e87926e64cb291429b56ce443a73ea28f48a06
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-16 23:34:58 -0700

    Implemented  Jump Stamina Cost, but it's buggy

--- src/StarterCharacterScripts/CustomMovementWithSFX/Main.client.lua
+++ src/StarterCharacterScripts/CustomMovementWithSFX/Main.client.lua
@@ -23,6 +23,7 @@ local PlayerGui = player:WaitForChild("PlayerGui")
 --Modules
 local Sprint = require(script.Parent.Modules:WaitForChild("Sprint"))
 local Crouch = require(script.Parent.Modules:WaitForChild("Crouch"))
+local StaminaManager = require(script.Parent.Modules:WaitForChild("StaminaManager"))
 local AnimationManager = require(script.Parent.Modules:WaitForChild("AnimationManager"))
 
 --This handles the animations. It plays/stops certain animation tracks when the humanoid is moving at a certain speed.
@@ -30,8 +31,19 @@ humanoid.Running:Connect(function(speed)
     AnimationManager.sprintAnimHandler(speed)
 end)
 
+local JUMP_STAMINA_COST = StaminaManager.MAX_STAMINA * 0.3
+local SLIDE_STAMINA_COST
+
 humanoid.Jumping:Connect(function()
-	
+	if StaminaManager.getCurrentStamina() > JUMP_STAMINA_COST then
+		print("checkoint 90")
+		humanoid.JumpHeight = 4
+		StaminaManager.updateStaminaBar(StaminaManager.getCurrentStamina() - JUMP_STAMINA_COST/2) --Jump stamina cost is divided by 2 here because the humanoid.Jumping event fires twice (once for when the jump starts and once for when the jump ends), but I only want to apply the JUMP_STAMINA_COST once per jump.
+		StaminaManager.fillStaminaBar()
+	else
+		print("checkpoint 91")
+		humanoid.JumpHeight = 0
+	end
 end)
 
 humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
--- src/StarterCharacterScripts/CustomMovementWithSFX/Modules/StaminaManager.lua
+++ src/StarterCharacterScripts/CustomMovementWithSFX/Modules/StaminaManager.lua
@@ -1,15 +1,90 @@
 local player = game.Players.LocalPlayer
+local character = player.Character
+local humanoid = character:WaitForChild("Humanoid")
 local PlayerGui = player:WaitForChild("PlayerGui")
 local CharacterStatusGui = PlayerGui:WaitForChild("CharacterStatusGui")
 
-local MAXSTAMINA = 100
-local currentStamina = MAXSTAMINA
-local minRequiredStamina = 15 --this is a percentage
+local CharacterSpeedInfo = require(script.Parent.CharacterSpeedInfo)
+
+local MAX_STAMINA = 100
+local currentStamina = MAX_STAMINA
+local MIN_REQUIRED_STAMINA = 15 --this is a percentage
 local staminaBar = CharacterStatusGui:WaitForChild("staminaDisplay"):WaitForChild("bgFrame"):WaitForChild("staminaFrame")
 local minRequiredStaminaBar = staminaBar:Clone()
-minRequiredStaminaBar.Size = UDim2.new(1*(minRequiredStamina/100), 0, 1, 0)
+minRequiredStaminaBar.Size = UDim2.new(1*(MIN_REQUIRED_STAMINA/100), 0, 1, 0)
 minRequiredStaminaBar.ZIndex = 2
 minRequiredStaminaBar.BackgroundColor3 = Color3.new(0, 0, 0)
 minRequiredStaminaBar.BackgroundTransparency = 0.8
 minRequiredStaminaBar.Parent = staminaBar.Parent
 local staminaLabel = CharacterStatusGui:WaitForChild("staminaDisplay"):WaitForChild("bgFrame"):WaitForChild("staminaLabel")
+local drainConnection
+local fillConnection
+local RunService = game:GetService("RunService")
+
+print("checkpoint")
+
+local StaminaManager = {
+    MAX_STAMINA = MAX_STAMINA,
+    MIN_REQUIRED_STAMINA = MIN_REQUIRED_STAMINA
+}
+
+function StaminaManager.getCurrentStamina()
+    return currentStamina
+end
+
+function StaminaManager.updateStaminaBar(newStaminaValue : number)
+    currentStamina = math.clamp(newStaminaValue, 0, MAX_STAMINA) --math.clamp ensures currentStamina doesn't go below 0
+    staminaLabel.Text = "Stamina: ".. math.round(currentStamina/MAX_STAMINA*100) .. "%" --displays the percent of stamina remaining rounded to the nearest whole #
+    staminaBar:TweenSize(UDim2.new(currentStamina/MAX_STAMINA, 0, 1, 0), "Out", "Linear", 0)
+end
+
+function StaminaManager.drainStaminaBar()
+    --If the stamina bar is regenerating, then stop it
+    if fillConnection then
+        fillConnection:Disconnect()
+        fillConnection = nil
+    end
+
+    --drains the stamina bar
+    if drainConnection == nil then
+        drainConnection = RunService.RenderStepped:Connect(function(dt)
+            --print(currentStamina .. "-drain")
+            --while the player has more than 0 stamina, the player will be able to sprint, but their stamina bar will deplete
+            if currentStamina > 0 then
+                StaminaManager.updateStaminaBar(currentStamina - 10*dt)
+            else
+                --When the player reaches 0 stamina, the stamina bar will no longer deplete and the player will be set back to walking speed
+                drainConnection:Disconnect()
+                drainConnection = nil
+                humanoid.WalkSpeed = CharacterSpeedInfo.walkSpeed
+            end
+        end)
+    end
+end
+
+function StaminaManager.fillStaminaBar()
+    --If the stamina bar is depleting, then stop it
+    if drainConnection then
+        drainConnection:Disconnect()
+        drainConnection = nil
+    end
+    
+    if fillConnection == nil then
+        --Regenerates the stamina bar
+        fillConnection = RunService.RenderStepped:Connect(function(dt)
+            --print(currentStamina .. "-fill")
+            --If the player's stamina is less than their stamina cap, then their stamina bar will regenerate
+            if currentStamina < MAX_STAMINA then
+                StaminaManager.updateStaminaBar(currentStamina + 10*dt)
+            else
+                --the stamina bar will stop regenerating once it's reached max stamina
+                if fillConnection then
+                    fillConnection:Disconnect()
+                    fillConnection = nil
+                end
+            end
+        end)
+    end
+end
+
+return StaminaManager
