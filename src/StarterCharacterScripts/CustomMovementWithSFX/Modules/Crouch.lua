1f69eb4129736590ca063d00c3db2725b6ca15da
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-19 11:09:23 -0700

    Organized Crouch.lua

--- src/StarterCharacterScripts/CustomMovementWithSFX/Modules/Crouch.lua
+++ src/StarterCharacterScripts/CustomMovementWithSFX/Modules/Crouch.lua
@@ -1,9 +1,14 @@
+------------------------------------------------------------------------<<<LOCAL VARIABLES>>>
 local CROUCH_KEY = Enum.KeyCode.C
 
+------------------------------------------------------------------------<<<PLAYER SPECIFICS>>>
 local player = game.Players.LocalPlayer --Because of this line, this module must be required in a LocalScript, else there will be an error.
 local humanoid = player.Character:WaitForChild("Humanoid")
+
+------------------------------------------------------------------------<<<MODULES>>>
 local CharacterSpeedInfo = require(script.Parent.CharacterSpeedInfo)
 
+------------------------------------------------------------------------<<<MODULE SCRIPT>>>
 local Crouch = {CROUCH_KEY = CROUCH_KEY}
 
 function Crouch.crouchKeyDown()
