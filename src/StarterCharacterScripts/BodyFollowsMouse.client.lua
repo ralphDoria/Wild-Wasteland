c843ef9958a2dd926671f3ece48584550bb567cd
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-06-20 19:36:42 -0700

    Update BodyFollowsMouse.client.lua

--- src/StarterCharacterScripts/BodyFollowsMouse.client.lua
+++ src/StarterCharacterScripts/BodyFollowsMouse.client.lua
@@ -1,6 +1,8 @@
+------------------------------------------------------------------------<<<PLAYER SPECIFICS>>>
 local player = game:GetService("Players").LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
+------------------------------------------------------------------------<<<LOCAL VARIABLES>>>
 local M6Ds = {
     waist : Motor6D
     rightHip : Motor6D
@@ -17,3 +19,6 @@ local M6Ds = {
 --V
 
 local c0Origin = {} --this is so we know the default positions for each Motor6D
+
+------------------------------------------------------------------------<<<ROBLOX LIBRARIES>>>
+local RunService = game:GetService("RunService")
