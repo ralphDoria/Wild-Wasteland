11ea95b682944190040f7de78b42fbd3db25f17b
Niletheus <120857955+Niletheus@users.noreply.github.com>
2024-12-17 19:20:14 -0800

    Wally: installed + figuring out how dependencies work

--- src/ServerScriptService/NPC_Manager/npc_script_executor.server.lua
+++ src/ServerScriptService/NPC_Manager/npc_script_executor.server.lua
@@ -2,6 +2,8 @@ local tag = "npc_mutantRoach"
 local targetPart = game.Workspace.npc_roach_test.targetLocation
 local CollectionService = game:GetService("CollectionService")
 local mutantRoach = require(script.Parent.mutantRoach)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local trove = require(ReplicatedStorage.Packages.Trove)
 
 local function mutantRoachTagHandler(taggedInstance)
     local roach = mutantRoach.new(taggedInstance)
