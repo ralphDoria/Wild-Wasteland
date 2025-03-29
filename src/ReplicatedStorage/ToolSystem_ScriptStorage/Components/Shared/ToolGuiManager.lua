6d14ba473aa83b225fdce30d0e89fff7b8e41e1a
Niletheus <120857955+Niletheus@users.noreply.github.com>
2025-03-28 19:58:32 -0700

    New ToolGuiManager WIP 2

--- src/ReplicatedStorage/ToolSystem_ScriptStorage/Components/Shared/ToolGuiManager.lua
+++ src/ReplicatedStorage/ToolSystem_ScriptStorage/Components/Shared/ToolGuiManager.lua
@@ -3,11 +3,12 @@ local player = game:GetService("Players").LocalPlayer
 local playerGui = player.PlayerGui
 local InputCategorizer = require(game:GetService("ReplicatedStorage").RojoManaged_RS.ActionManagerSystem.Components.InputCategorizer)
 
-local ToolGui = playerGui:WaitForChild("ToolGui")
-local AmmoInfo: Frame = ToolGui.Frame.AmmoInfo
+local ToolGui: ScreenGui = playerGui:WaitForChild("ToolGui")
+local frame: Frame = ToolGui:FindFirstChild("Frame"):: Frame
+local AmmoInfo: Frame = frame:FindFirstChild("AmmoInfo"):: Frame
 local loaded: TextLabel = AmmoInfo:FindFirstChild("Loaded"):: TextLabel
 local unloaded: TextLabel = AmmoInfo:FindFirstChild("Unloaded"):: TextLabel
-local Toolinfo: Frame = ToolGui.Frame.ToolInfo
+local Toolinfo: Frame = frame:FindFirstChild("ToolInfo"):: Frame
 local name: TextLabel = Toolinfo:FindFirstChild("Name"):: TextLabel
 local image: ImageLabel = Toolinfo:FindFirstChild("Image"):: ImageLabel
 
@@ -20,11 +21,55 @@ local ToolGuiManager = {
     _initialized = false
 }
 
-function ToolGuiManager.initialize(self: ToolGuiManager)
-end
-
 function ToolGuiManager.toggleToolGuiVisibility(self : ToolGuiManager, tool : Tool, toggle : boolean)
     ToolGui.Enabled = toggle
+end
+
+function ToolGuiManager.setTool(tool: Tool)
+    if tool:HasTag("Gun") then
+        AmmoInfo.Visible = true
+        loaded.Text = tostring(tool:GetAttribute("Loaded"))::string
+        unloaded.Text = tostring(tool:GetAttribute("Unloaded"))::string
+        -- Set loaded and unloaded labels here
+    else
+        AmmoInfo.Visible = false
+        name.Text = tool.Name
+        image.Image = tool:GetAttribute("ImageId"):: string -- This right here is a place holder
+    end
+end
+
+function ToolGuiManager._updatePositionAndScale()
+    if InputCategorizer.getLastInputCategory() == "Touch" then
+        -- Position gui in upper right corner
+        ToolGui
+    else
+        -- Position gui in bottom right corner
+
+    end
+end
+
+function ToolGuiManager.initialize(self: ToolGuiManager)
+    -- Update the position and scale of the list if the TouchGui is added/removed
+	playerGui.ChildAdded:Connect(function(child)
+		if child.Name == "TouchGui" then
+			ToolGuiManager._updatePositionAndScale()
+		end
+	end)
+
+	playerGui.ChildRemoved:Connect(function(child)
+		if child.Name == "TouchGui" then
+			ToolGuiManager._updatePositionAndScale()
+		end
+	end)
+
+	-- Update the displayed buttons when the input category changes
+	InputCategorizer.lastInputCategoryChanged:Connect(function(inputCategory)
+		ToolGuiManager._updatePositionAndScale()
+	end)
+
+	-- Update the position and scale of the list when the screen size changes or last input category changes
+	ToolGui:GetPropertyChangedSignal("AbsoluteSize"):Connect(ToolGuiManager._updatePositionAndScale)
+	InputCategorizer.lastInputCategoryChanged:Connect(ToolGuiManager._updatePositionAndScale)
 end
 
 return ToolGuiManager
